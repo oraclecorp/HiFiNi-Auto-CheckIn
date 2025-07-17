b4f7f808000a0d97fc188536ec0da845d63e3706
oraclecorp <liqin.zhang@gmail.com>
2025-07-17 11:00:12 +0800

    update var

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -190,7 +190,8 @@ public class Main {
         }
 
         // 使用正则表达式匹配sign变量的值
-        Pattern pattern = Pattern.compile("var s1 = \"([^\"]+)\"");
+        //Pattern pattern = Pattern.compile("var s1 = \"([^\"]+)\"");
+        Pattern pattern = Pattern.compile("var s1 = \'([^\"]+)\'");
         Matcher matcher = pattern.matcher(signPageCode);
 
         if (matcher.find()) {
